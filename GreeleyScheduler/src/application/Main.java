c0dfff492899aa3c24973d3a5e91b107f2b8aee2
Michael Maitland <michaelmaitland@verizon.net>
2017-03-11 11:28:33 -0500

    Update Main.java

--- GreeleyScheduler/src/application/Main.java
+++ GreeleyScheduler/src/application/Main.java
@@ -1,25 +1,39 @@
 package application;
 
 /**
+ * This class is responsible for spawning the GUI so it can take care of the program
+ * 
  * @author yasgur99
  **/
 
 import javax.swing.SwingUtilities;
 import application.gui.UserInterface;
-import application.logic.PDFParser;
+import application.logic.ExcelParser;
+import application.logic.MasterSchedule;
 import javax.swing.UIManager;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        /*Setup data*/
-        PDFParser parser = new PDFParser("resources/master schedule 15-16 from IC.pdf");
-        
+    /**
+     * Initializes the GUI and leaves it up to the GUI to execute the application as intended
+     * @param args
+     * @throws Exception from setting the look and feel as the system look and feel
+     */
+    public static void main(String[] args) throws Exception{
         /*Set look and feel of gui*/
         UIManager.setLookAndFeel(
             UIManager.getSystemLookAndFeelClassName());
         
         /*Create new UI and invoke it when possible*/
         UserInterface ui = new UserInterface();
-        SwingUtilities.invokeLater(ui);
+        //SwingUtilities.invokeLater(ui);
+        
+        /*Setup data*/
+        String[] filenames = {"resources/2016-2017 Art & Life Scheduling Projections.xlsx"};
+        ExcelParser parser = new ExcelParser(filenames);
+        
+        /*Create Master Schedule with a population of 1000*/
+        MasterSchedule schedule = new MasterSchedule(parser.getSections(), 1000);
+        schedule.run();
     }
 }
