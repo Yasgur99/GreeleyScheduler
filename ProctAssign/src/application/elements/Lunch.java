03061151423761b8cf64e6f74b7a0b846d82111c
Michael Maitland <michaelmaitland@Michaels-MBP-2.fios-router.home>
2017-03-10 19:51:14 -0500

    Remove ProctAssign

--- ProctAssign/src/application/elements/Lunch.java
+++
@@ -1,5 +0,0 @@
-package application.elements;
-
-public enum Lunch{
-    EARLY, MIDDLE, LUNCH;
-}
--- ProctAssign/src/application/elements/Subject.java
+++
@@ -1,5 +0,0 @@
-package application.elements;
-
-public enum Subject{
-    ARTS_AND_LIFE_SKILLS, LEARNING_CENTERS, MATH, SCIENCE, ENGLISH, SOCIAL_STUDIES, PE, WORLD_LANGUAGE;
-}
